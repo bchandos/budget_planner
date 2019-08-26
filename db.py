7d3f9971171c00e9bbef073c64250be2204020d6
Bill Chandos <bill.chandos@gmail.com>
2019-08-25 17:14:05 -0700

    creating db and session management

--- db.py
+++ db.py
@@ -1,23 +1,25 @@
-# handle all the database setup or something
 from contextlib import contextmanager
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# an Engine, which the Session will use for connection
-# resources
+from models import Base
+
+# an Engine, which the Session will use for connection resources
 engine = create_engine('sqlite:///budget.db')
 
 # create a configured "Session" class
 Session = sessionmaker(bind=engine)
 
+# create tables if they do not exist
+Base.metadata.create_all(bind=engine)
 
 @contextmanager
 def session_scope():
-    """    Provide a transactional scope around a series of operations.
+    """Provide a transactional scope around a series of operations.
     Usage:
-    with session_scope() as session:
-        session.query(...)
+        with session_scope() as session:
+            session.query(...)
     """
 
     session = Session()
--- import_transactions.py
+++ import_transactions.py
@@ -3,7 +3,8 @@ import os
 import re
 
 from json_settings import JSONSettings
-from models import Transaction
+from models import Transaction, Account
+from db import session_scope
 
 settings = JSONSettings(
     'settings.json', settings_template='settings-example.json')
@@ -42,12 +43,14 @@ def import_all_transactions(csv_dict, bank):
     """
 
     field_matchings = settings.get_setting('file_formats', 'bank')['field_matchings']
-    for row in csv_dict:
-        transaction = Transaction(account=row.get(field_matchings.get('account')),
-                                date=row.get(field_matchings.get('date')),
-                                description=row.get(field_matchings.get('description')),
-                                category=row.get(field_matchings.get('category')),
-                                credit=row.get(field_matchings('credit')),
-                                debit=row.get(field_matchings('debit')),
-                                amount=None)
-        transaction.save()
+    with session_scope() as session:    
+        for row in csv_dict:
+            account = session.query(Account, name=field_matchings.get('account'))
+            transaction = Transaction(account=account,
+                                    date=row.get(field_matchings.get('date')),
+                                    description=row.get(field_matchings.get('description')),
+                                    category=row.get(field_matchings.get('category')),
+                                    credit=row.get(field_matchings('credit')),
+                                    debit=row.get(field_matchings('debit')),
+                                    amount=None)
+            session.add(transaction)
