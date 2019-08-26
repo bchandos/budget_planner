1b26e6ac92efee3ffbea1bb4be17e867b0010b0d
Bill Chandos <bill.chandos@gmail.com>
2019-08-25 18:52:39 -0700

    fixing all the broken shit

--- import_transactions.py
+++ import_transactions.py
@@ -1,6 +1,7 @@
 import csv
 import os
 import re
+from datetime import datetime, date
 
 from json_settings import JSONSettings
 from models import Transaction, Account
@@ -30,7 +31,7 @@ def parse_upload(file_object):
 def import_all_transactions(csv_dict, bank):
     """
     Given the DictReader results from the csv file import, as well as 
-    the bank id, imports transactions to database. Uses bank's field
+    the bank name, imports transactions to database. Uses bank's field
     matching attributes.
     
     :param csv_dict: DictReader results from parse_upload
@@ -42,15 +43,24 @@ def import_all_transactions(csv_dict, bank):
     :raise Exception: raises an exception
     """
 
-    field_matchings = settings.get_setting('file_formats', 'bank')['field_matchings']
+    field_matchings = settings.get_setting('file_formats', bank)['field_matchings']
     with session_scope() as session:    
         for row in csv_dict:
-            account = session.query(Account, name=field_matchings.get('account'))
-            transaction = Transaction(account=account,
-                                    date=row.get(field_matchings.get('date')),
+            account = session.query(Account).filter(Account.name==bank).one_or_none()
+            trans_date = datetime.strptime(row.get(field_matchings.get('date')), settings.get_setting('file_formats', bank)['datetime_format'])
+            if row.get(field_matchings.get('credit')):
+                credit = float(row.get(field_matchings.get('credit')))
+            else:
+                credit = 0
+            if row.get(field_matchings.get('debit')):
+                debit = float(row.get(field_matchings.get('debit')))
+            else:
+                debit = 0
+            transaction = Transaction(account=account.id,
+                                    date=trans_date.date(),
                                     description=row.get(field_matchings.get('description')),
                                     category=row.get(field_matchings.get('category')),
-                                    credit=row.get(field_matchings('credit')),
-                                    debit=row.get(field_matchings('debit')),
+                                    credit=credit,
+                                    debit=debit,
                                     amount=None)
             session.add(transaction)
