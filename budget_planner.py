866e94ce2533213fa41e8b0c6b3130f9907f5bf7
Bill Chandos <bill.chandos@gmail.com>
2019-09-16 20:46:04 -0700

    add response codes,, transaction delete related transactions begin account creation/delete/edit

--- budget_planner.py
+++ budget_planner.py
@@ -4,7 +4,8 @@ import json
 from datetime import date
 
 import dateparser
-from bottle import Bottle, redirect, request, run
+from bottle import Bottle, redirect, request, run, response
+from sqlalchemy import or_
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
 from sqlalchemy.inspection import inspect
@@ -71,11 +72,12 @@ def transactions(account_id=None):
         else:
             status = 'failed'
             payload = {f'no account id {account_id}'}
+            response.status = 404
             
     return {'status': status, 'payload': payload}
 
 @app.route(f'{API_V}/transaction', method='POST')
-@app.route(f'{API_V}/transaction/<trans_id:int>', method=('GET', 'PUT'))
+@app.route(f'{API_V}/transaction/<trans_id:int>', method=('GET', 'PUT', 'DELETE'))
 def transaction(trans_id=None):
     if request.method == 'GET' and trans_id:
         with session_scope() as session:
@@ -87,6 +89,7 @@ def transaction(trans_id=None):
                 else:
                     status = 'failed'
                     payload = {'error_message': f'no transaction id {trans_id}'}
+                    response.status = 404
     
     elif request.method == 'POST' and request.json:
         # create a new transaction, ignores trans_id
@@ -103,12 +106,13 @@ def transaction(trans_id=None):
         if not parsed_date:
             status = 'failed'
             payload = {'error_message': 'date missing or invalid format'}
+            response.status = 400
             return {'status': status, 'payload': payload}
         else:
             json_request['date'] = parsed_date                              #pylint: disable=unsupported-assignment-operation
         # make sure we supply a credit value, if not passed, to enforce UNIQUE constraint
-        if not json_request.get('credit'):
-            json_request['credit'] = 0
+        if not json_request.get('credit'):                                  #pylint: disable=no-member
+            json_request['credit'] = 0                                      #pylint: disable=unsupported-assignment-operation
         with session_scope() as session:
             new_transaction = Transaction(**json_request)                   #pylint: disable=not-a-mapping
             try:
@@ -119,21 +123,18 @@ def transaction(trans_id=None):
                 status = 'failed'
                 error_message = str(err).split(':')[0]
                 payload = {'error_message': error_message}
+                response.status = 400
             else:
                 status = 'success'
                 payload = new_transaction._asdict('date', 'credit', 'debit', 'amount')
+                response.status = 201
 
     elif request.method == 'PUT' and trans_id and request.json:
         # update existing transaction id = trans_id
         json_request = request.json
         # format the date
-        parsed_date = dateparser.parse(json_request.get('date'))            #pylint: disable=no-member
-        if not parsed_date:
-            status = 'failed'
-            payload = {'error_message': 'date missing or invalid format'}
-            return {'status': status, 'payload': payload}
-        else:
-            json_request['date'] = parsed_date                              #pylint: disable=unsupported-assignment-operation
+        if json_request.get('date'):            #pylint: disable=no-member
+            json_request['date'] = dateparser.parse(json_request.get('date'))  #pylint: disable=unsupported-assignment-operation,no-member
         # should not be able to update an id
         if json_request.get('id'):                                          #pylint: disable=no-member
             json_request.pop('id')                                          #pylint: disable=no-member
@@ -151,32 +152,83 @@ def transaction(trans_id=None):
                     status = 'success'
                     transaction = session.query(Transaction).get(trans_id)
                     payload = transaction._asdict('date', 'credit', 'debit', 'amount')
+                    response.status = 201
                 else:
                     status = 'failed'
                     payload = {'error_message': f'no transaction id {trans_id}'}
+                    response.status = 404
+    elif request.method == 'DELETE' and trans_id:
+        with session_scope() as session:
+            del_rows = session.query(Transaction).filter(Transaction.id==trans_id).delete()
+            if del_rows > 1:
+                session.rollback()
+        if del_rows == 1:
+            status = 'success'
+            payload = {'message': f'successfully deleted transaction id {trans_id}'}
+            response.status = 200
+        else:
+            status = 'failed'
+            payload = {'error_message': f'could not delete or does not exist id {trans_id}'}
+            response.status = 404
+
     else:
         status = 'failed'
         payload = {'error_message': 'not a valid request'}
+        response.status = 400
     return {'status': status, 'payload': payload}
 
-@app.route(f'{API_V}/accounts')
-@app.route(f'{API_V}/accounts/<account_id:int>')
+@app.route(f'{API_V}/accounts', method=('GET', 'POST'))
+@app.route(f'{API_V}/accounts/<account_id:int>', method=('GET', 'PUT', 'DELETE'))
 def accounts(account_id=None):
     with session_scope() as session:
-        if account_id:
-            accounts = session.query(Account).get(account_id)
-            status = 'success'
-            payload = accounts._asdict()
-        else:
+        if account_id: 
+            if request.method == 'GET':
+                # retrieve account by id
+                accounts = session.query(Account).get(account_id)
+                if accounts:
+                    status = 'success'
+                    payload = accounts._asdict()
+                    response.status = 200
+            elif request.method == 'PUT':
+                # update existing account
+                pass
+            elif request.method == 'DELETE':
+                # delete existing account
+                pass
+
+        elif request.method == 'GET':
+            # get all accounts
             accounts = session.query(Account).all()
             status = 'success'
             payload = [row._asdict() for row in accounts]
-        
+            response.status = 200
+        elif request.method == 'POST':
+            # create new account
+            pass
+
         if not accounts:
             status = 'failed'
-            payload = {f'no account id {account_id}'}
+            payload = {'error_message': f'no account id {account_id}'}
+            response.status = 404
             
     return {'status': status, 'payload': payload}
 
+@app.route(f'{API_V}/transaction/<trans_id:int>/related', method='GET')
+def related_transactions(trans_id):
+    with session_scope() as session:
+        orig_trans = session.query(Transaction).get(trans_id)
+        related_trans = session.query(Transaction).filter(Transaction.reconcile_to==trans_id).all()
+        if orig_trans:
+            status = 'success'
+            payload = dict()
+            payload['original_transaction'] = orig_trans._asdict('date', 'credit', 'debit', 'amount')
+            payload['related_transactions'] = [row._asdict('date', 'credit', 'debit', 'amount') for row in related_trans]
+            response.status = 200
+        else:
+            status = 'failed'
+            payload = f'no transaction or related transactions id {trans_id}'
+            response.status = 404
+
+        return {'status': status, 'payload': payload}
 
 run(app)
