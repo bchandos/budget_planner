0197c39db320f52f1df6fbe7754f20fbf1d0ea1e
Bill <bill.chandos@gmail.com>
2020-11-23 00:12:43 +0000

    god I'm dumb

--- budget_planner.py
+++ budget_planner.py
@@ -372,4 +372,4 @@ def categories():
     
     return {'status': status, 'payload': payload}
 
-run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000))), server='gunicorn')
+run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), server='gunicorn')
